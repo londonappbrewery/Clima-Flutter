9b7bb5d8c69a124616bea4c022cfde8e4fde0d5b
Frank Liao <fliao@digitaltransformr.com>
2019-10-04 03:36:10 -0700

    Completed Module 120

---
+++ lib/scratch.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+  String result;
+
+  Duration threeSeconds = Duration(seconds: 3);
+  await Future.delayed(threeSeconds, () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+
+  return result;
+}
+
+void task3(String task2Data) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $task2Data');
+}
