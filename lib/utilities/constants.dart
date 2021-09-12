352dcc4cfaf319993a8b01f126e61a865b7ec230
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-09-12 16:36:21 +0530

    new version of clima app

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -3,18 +3,37 @@ import 'package:flutter/material.dart';
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
   fontSize: 100.0,
+  color: Color(0xFFf4f5f2),
 );
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 50.0,
+  color: Color(0xFFf4f5f2),
 );
 
 const kButtonTextStyle = TextStyle(
   fontSize: 30.0,
   fontFamily: 'Spartan MB',
+  color: Color(0xFFe6ebe1),
 );
 
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Color(0xFFfafafa),
+  icon: Icon(Icons.location_city, color: Colors.white),
+  hintText: "Enter City Name",
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
 );
